6d0079f7602572875ad9ea9bd0c87f45e690b439
Nindza007 <115565512+Nindza007@users.noreply.github.com>
2022-11-05 15:17:23 +0300

    test
    test

--- back/db/migrations/env.py
+++ back/db/migrations/env.py
@@ -15,7 +15,6 @@ from db import Base
 # access to the values within the .ini file in use.
 config = context.config
 
-
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
@@ -28,19 +27,7 @@ target_metadata = Base.metadata
 config.set_main_option("sqlalchemy.url", settings.SQLALCHEMY_URL)
 
 
-
 def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode.
-
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
-
-    Calls to context.execute() here emit the given string to the
-    script output.
-
-    """
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
@@ -61,12 +48,6 @@ def do_run_migrations(connection: Connection) -> None:
 
 
 async def run_migrations_online() -> None:
-    """Run migrations in 'online' mode.
-
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
-
-    """
     connectable = AsyncEngine(
         engine_from_config(
             config.get_section(config.config_ini_section),
